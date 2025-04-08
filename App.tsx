92b855dab39f7b2e1be74236a8105b3b469bb5ee
leonardo-Arayad <leonardo.araya.d@hotmail.com>
2025-04-08 12:12:01 -0400

    actualizacion lista de cards

--- App.tsx
+++ App.tsx
@@ -14,38 +14,74 @@ export default function App() {
   const [cardsData, setCardsData] = useState([
     {
       titulo: 'Leonardo Araya',
-      contenido: "Mi área profesional es la informática(desarrollo de software).Mis aptitudes van desde la ingeniería hasta la edición gráfica y creación de contenido, pasando por mi lado creativo.Como Desarrollador con más de 2 años de experiencia, he desarrollado una profunda comprensión de las tecnologías de UI y he demostrado habilidades en la creación de interfaces atractivas y fáciles de usar.Utilicé ampliamente frameworks como React y tengo un conocimiento sólido en HTML, CSS y JavaScript.También tengo una comprensión clara de la importancia del diseño y la usabilidad, y he trabajado en colaboración con equipos de diseño para asegurar una experiencia de usuario positiva.Soy capaz de resolver problemas de manera eficiente y estoy constantemente buscando nuevas formas de mejorar mis habilidades y aprender nuevas tecnologías.",
+      contenido: "Mi área profesional es la informática (desarrollo de software). Mis aptitudes van desde la ingeniería hasta la edición gráfica y creación de contenido, pasando por mi lado creativo. Como Desarrollador con más de 4 años de experiencia, he desarrollado una profunda comprensión de las tecnologías de UI y he demostrado habilidades en la creación de interfaces atractivas y fáciles de usar. He trabajado extensamente con frameworks como React, Angular, Flutter y tecnologías como HTML, CSS, JavaScript y TypeScript. También tengo experiencia en metodologías ágiles, control de versiones, consumo de APIs REST y buenas prácticas de desarrollo.",
       source: 'https://media.licdn.com/dms/image/v2/C4E03AQGY3mbtVgEkmQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1611866750459?e=1743033600&v=beta&t=BiTG2e5oTZkt6I0GWPMIn0sTNauOg6tyaGZNAtlTZVg',
       isExpanded: true,
     },
     {
+      titulo: 'Correos de Chile',
+      contenido: [
+        "Desarrollo Front-End con React y TypeScript",
+        "Consumo y depuración de APIs REST",
+        "Control de versiones con GitLab",
+        "Implementación de metodologías ágiles y gestión con Jira",
+        "Garantía de calidad del código con SonarQube",
+      ],
+      source: 'https://play-lh.googleusercontent.com/ShyxSvhWEsqFSYJQ1yWZpzxOMYNAPijkwO_gGrUVRnc90RIT1MIavcc8WY1JSWOQfA=w240-h480',
+      date: '01/2024 - 12/2024',
+      isExpanded: false,
+    },
+    {
+      titulo:'Dhemax Ingenieros',
+      contenido: [
+        "Desarrollo y mantenimiento de soluciones con React.js, TypeScript y Redux Toolkit",
+        "Integración de APIs y normalización de datos",
+        "Uso de herramientas como Postman y Swagger para pruebas de endpoints",
+        "Control de versiones con Git y GitLab",
+      ],
+      source: 'https://www.developmentaid.org/files/organizationLogos/dhemax-ingenieros-spa-590907.jpg',
+      date: '01/2023 - 12/2023',
+      isExpanded: false,
+    },
+    {
+      titulo: 'MITs IT/INDRA',
+      contenido: [
+        "Desarrollo Front-End en React con TypeScript y Material UI",
+        "Automatización de tareas y flujos con Jest y Redux",
+        "Consumo de APIs REST y control de estados",
+        "Metodologías ágiles con Jira y Scrum",
+      ],
+      source: 'https://www.indraairsolutions.com/themes/custom/atm_sass/l-Logo.svg',
+      date: '07/2022 - 12/2022',
+      isExpanded: false,
+    },
+    {
+      titulo: 'Fractalia Systems',
+      contenido: [
+        "Desarrollo Front-End en Angular 11",
+        "Diseño de interfaces con HTML, CSS y Sass",
+        "Control de versiones con Git y Bitbucket",
+        "Metodologías ágiles: Kanban y Scrum",
+      ],
+      source: 'https://fractaliasystems.com/wp-content/uploads/2018/09/logo_grupo_fractalia.png',
+      date: '06/2021 - 06/2022',
+      isExpanded: false,
+    },
+    {
       titulo: 'Banco de Chile',
       contenido: [
-        "Implementation of new features with Angular 8",
-        "Mobile app development for executives using Dart and Flutter",
-        "Code versioning with Git and Bitbucket",
-        "Task administration with Jira",
-        "Execution of Scrum methodology",
-        "Use of Jenkins for CI/CD",
+        "Desarrollo móvil para ejecutivos con Dart y Flutter",
+        "Implementación de funcionalidades en Angular 8",
+        "Integración continua con Jenkins",
+        "Administración de tareas con Jira",
+        "Control de versiones con Bitbucket",
       ],
       source: 'https://pbs.twimg.com/profile_images/1694746966058496010/Y48o3RBX_400x400.jpg',
       date: '02/2020 - 12/2020',
       isExpanded: false,
     },
-    {
-      titulo: 'Correos de Chile',
-      contenido: [
-        "Development of modern web applications using React and TypeScript.",
-        "Code version control using GitLab",
-        "Consumption and debugging of REST APIs",
-        "Implementation of agile methodologies and task management with Jira",
-        "Code quality assurance with SonarQube.",
-      ],
-      source: 'https://play-lh.googleusercontent.com/ShyxSvhWEsqFSYJQ1yWZpzxOMYNAPijkwO_gGrUVRnc90RIT1MIavcc8WY1JSWOQfA=w240-h480',
-      date: '02/2020 - 12/2020',
-      isExpanded: false,
-    },
-  ])
+  ]);
+  
   const [refreshing, setRefreshing] = useState(false);
 
 
